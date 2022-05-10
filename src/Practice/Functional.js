67c58b18f90cf0acbe659ec767ed94a6bedb0d95
Basheerbachi <85440600+Basheerbachi@users.noreply.github.com>
2022-05-10 19:23:42 +0530

    Add files via upload

---
+++ src/Practice/Functional.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Functional = () => {
+  return (
+     <div id="boxFunc">
+          <h3 style={{marginTop:"1vw",marginBottom:"1vw"}}>Made using Functional Component</h3>
+          <p>This is done by using external CSS</p>
+          <p style={{color:"red"}}>This is done by using internal CSS</p>
+     </div>
+  )
+}
+
+export default Functional
